304badc7f7034beb0cc107e54439b6bc57ee7d45
MartinBrave <matin.shoja23@gmail.com>
2021-03-04 09:55:27 +0330

    restarting

--- account/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from account.models import Account
-
-
-@admin.register(Account)
-class PostAdmin(admin.ModelAdmin):
-    list_filter = ('username',)
-    list_display = ('username', 'first_name', 'last_name')
--- account/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Account(models.Model):
-    first_name = models.CharField(max_length=127, null=False)
-    last_name = models.CharField(max_length=127, null=False)
-    username = models.CharField(max_length=127, null=False)
-    mail = models.EmailField(max_length=127, null=False)
-    password = models.CharField(max_length=127, null=False)
