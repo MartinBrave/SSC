2c1e7dbad4f2928a77cd98ccc3e449e92e5c3ccd
MartinBrave <matin.shoja23@gmail.com>
2021-03-04 09:01:14 +0330

    database

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from account.models import Account
+
+
+@admin.register(Account)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('username',)
+    list_display = ('username', 'first_name', 'last_name')
--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    first_name = models.CharField(max_length=127, null=False)
+    last_name = models.CharField(max_length=127, null=False)
+    username = models.CharField(max_length=127, null=False)
+    mail = models.EmailField(max_length=127, null=False)
+    password = models.CharField(max_length=127, null=False)
